95eef26e30dd15b559f5c1741c14d68cd761baaa
Emmanuel <102570049+garveygithubit@users.noreply.github.com>
2024-06-12 19:56:56 +0000

    Update Sidebar.jsx

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import { assets } from "../../assets/assets";
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);    
 
-  return (
+  return (   
     <div className="sidebar">
       <div className="top">
         <img onClick={()=>setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
